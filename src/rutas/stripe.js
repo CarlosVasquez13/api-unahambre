e33d11003c5d63c245c1e6e70cd78559c671b805
Carlos Vaquez <carlos1305vasquez@gmail.com>
2020-04-14 02:06:41 -0600

    Metodo de pago stripe actualizado

--- src/rutas/stripe.js
+++ src/rutas/stripe.js
@@ -10,16 +10,16 @@ const autenticar = require('../middlewares/autentication')
 // See your keys here: https://dashboard.stripe.com/account/apikeys
 const stripe = require('stripe')('sk_test_smJBw722C61JXaVJaIq4EwZl00rXkBU89f');
 
-router.get('/prueba_pago' , (req, res) => {
+router.post('/realizar_pago' , (req, res) => {
     (async () => {
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
             line_items: [{
-                name: 'T-shirt',
-                description: 'Comfortable cotton t-shirt',
-                images: ['https://example.com/t-shirt.png'],
-                amount: 50,
-                currency: 'usd',
+                name: req.body.nombre, //'T-shirt',
+                description: req.body.descripcion, //'Comfortable cotton t-shirt',
+                images: [req.body.foto], //['https://example.com/t-shirt.png'],
+                amount: req.body.monto,
+                currency: 'hnl',
                 quantity: 1,
             }],
             success_url: 'https://web-unahambre.herokuapp.com/principal.html',
