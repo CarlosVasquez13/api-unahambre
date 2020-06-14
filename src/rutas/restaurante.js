20b031933a8cf23c9f2505297ade3a2d816d7d76
Carlos Vaquez <carlos1305vasquez@gmail.com>
2020-06-14 02:03:36 -0600

    restaurante to Restaurante 2

--- src/rutas/restaurante.js
+++ src/rutas/restaurante.js
@@ -31,12 +31,12 @@ const enviar_correo = require('../models/mail_service')
  * el data.error llevará el mensaje de éxito o fracaso 
  */
 router.post('/g_insertar-restaurante', autenticar, function (req, res, next) {
-        const query = `CALL SP_INSERT_RESTAURANTE(?, ?, ?, ?, ?, ?, @MENSAJE); SELECT @MENSAJE AS mensaje;`
-        db.query(query, [req.body.idUsuario, req.body.rolUsuario, req.body.nombreRestaurante, req.body.telefono, req.body.correo, req.body.ubicacion],
-            function (err, result) {
-                if(!err) {
-                    // mensaje del correo a enviar al usuario
-                    var mensaje = `
+    const query = `CALL SP_INSERT_RESTAURANTE(?, ?, ?, ?, ?, ?, @MENSAJE); SELECT @MENSAJE AS mensaje;`
+    db.query(query, [req.body.idUsuario, req.body.rolUsuario, req.body.nombreRestaurante, req.body.telefono, req.body.correo, req.body.ubicacion],
+        function (err, result) {
+            if (!err) {
+                // mensaje del correo a enviar al usuario
+                var mensaje = `
                                      <div>
                                         <p>
                                             Estimado cliente, tu solicitud de resgistro del local ${req.body.nombreRestaurante} está en proceso. Le agradecemos por elegir trabajar con nosotros, pronto nos pondremos
@@ -49,17 +49,17 @@ router.post('/g_insertar-restaurante', autenticar, function (req, res, next) {
                                             
                                         </p>
                                     </div>
-                                    `    
-                    enviar_correo(mensaje, req.body.correo, res)
-                   
-                    /**
-                     * Nota_cambios: crear otro estado para el Restaurante y verificar el rol del cliente
-                     * Mejorar el mensaje a enviar al cliente
-                     * 
-                     */
-                                    
-                }
-                respuesta.respuestaError(err, result, res)
+                                    `
+                enviar_correo(mensaje, req.body.correo, res)
+
+                /**
+                 * Nota_cambios: crear otro estado para el Restaurante y verificar el rol del cliente
+                 * Mejorar el mensaje a enviar al cliente
+                 * 
+                 */
+
+            }
+            respuesta.respuestaError(err, result, res)
         })
 })
 
@@ -80,7 +80,7 @@ router.get('/g_mostrar_restaurantes', function (req, res, next) {
             ;`
     db.query(query,
         function (err, result) {
-            respuesta.respuestaItems(err, result, res)            
+            respuesta.respuestaItems(err, result, res)
         }
 
     );
@@ -111,7 +111,7 @@ router.get('/g_mostrar_platillos', function (req, res, next) {
                     WHERE EstadoRestaurante = "Activo" AND platillo.Estado = "A";`
     db.query(query,
         function (err, result) {
-           respuesta.respuestaItems(err, result, res)           
+            respuesta.respuestaItems(err, result, res)
         })
 });
 
@@ -125,7 +125,7 @@ router.get('/g_mostrar_platillos', function (req, res, next) {
  * retorna los datos del Restaurante y el propietario
  */
 router.post('/datos_restaurante_propietario', (req, res, next) => {
-    
+
     const query = `SELECT Restaurante.Nombre_Local, Restaurante.Foto_Restaurante, Restaurante.Correo, Restaurante.Telefono, Restaurante.Ubicacion, usuario.Foto_Perfil, persona.Nombre, persona.Apellidos FROM Restaurante INNER JOIN usuario 
                     ON Usuario_idUsuario = idUsuario
                     INNER JOIN persona
@@ -147,7 +147,7 @@ router.post('/menus_restaurante', (req, res, next) => {
     const query = `SELECT idMenu, Tipo_Menu, Foto_Menu FROM menu 
                     WHERE Restaurante_idRestaurante = ? AND menu.Estado = 'A'`;
     db.query(query, [req.body.idRestaurante],
-        function (err, result){
+        function (err, result) {
             respuesta.respuestaItems(err, result, res)
         })
 })
@@ -179,7 +179,7 @@ router.get('/banners', (req, res, next) => {
     INNER JOIN Restaurante 
     ON idRestaurante = Restaurante_idRestaurante;`
     db.query(query,
-        function(err, result) {
+        function (err, result) {
             if (!err) {
                 var array_publicidad = result
                 var rand_id = Math.floor(Math.random() * array_publicidad.length);
@@ -200,7 +200,7 @@ router.get('/pop_ups', (req, res, next) => {
         Platillo_idPlatillo FROM restaurante_has_publicidad WHERE Plan_idPlan = 2 OR Plan_idPlan = 1;`
     db.query(query,
         function (err, result) {
-            if(!err) {
+            if (!err) {
                 var array_publicidad = result
                 var rand_id = Math.floor(Math.random() * array_publicidad.length);
                 respuesta.respuestaItems(err, array_publicidad[rand_id], res)
